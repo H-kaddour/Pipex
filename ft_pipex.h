352cc57ae82a38b99f83893a1224f4291fc939d6
hichamkadd <banfoinformatique@gmail.com>
2022-01-19 19:56:23 +0100

    nnn

---
+++ ft_pipex.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipex.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkaddour <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 17:29:44 by hkaddour          #+#    #+#             */
+/*   Updated: 2022/01/19 17:47:03 by hkaddour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PIPEX_H
+# define FT_PIPEX_H
+# include <stdio.h>
+# include <unistd.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <sys/types.h>
+
+#endif
